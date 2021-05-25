e288372f093fbf7fadafe8bf5c67468a53cdd87b
Washington Sampaio Vieira <washingtonsampaio.23@gmail.com>
2021-05-24 23:53:02 -0300

    Adicionando model que faltou

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from modules.Talk import Talk
 from modules.User import  User
 from modules.Voluntary import Voluntary
 from modules.Donations import Donations
+from modules.Reception import Reception
 
 administrator =  Administrator(1)
 user = User("id_user", "name", "email", "password", "telefone", "'created_at'")
@@ -18,5 +19,6 @@ patient = Patient("name", "address", "diagnosis")
 report = Report("name", "url")
 voluntary = Voluntary("name", "cpf", "telphone")
 talk = Talk("local", "theme", "size", voluntary)
+reception = Reception("name", "address", "diagnosis")
 
 print("FOI")
